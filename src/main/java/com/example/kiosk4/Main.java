e82d7142a04151248827f1e2a73a3ba373b5d06d
나연 <kny5579@naver.com>
2025-01-13 20:42:39 +0900

    feat: LV 4

---
+++ src/main/java/com/example/kiosk4/Main.java
@@ -0,0 +1,28 @@
+package com.example.kiosk4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Menu 객체 생성하면서 카테고리 이름 설정
+        Menu burgerCategory = new Menu("Burgers");
+        //Menu 클래스 내 있는 List<MenuItem>에 MenuItem 객체 생성하면서 삽입
+        burgerCategory.addMenuItem(new MenuItem("빅맥", 0.6, "쇠고기 패티 두장, 소스, 치즈, 야채가 어우러진 버거"));
+        burgerCategory.addMenuItem(new MenuItem("쿼터파운더치즈", 0.7, "쇠고기 패티, 두꺼운 치즈, 야채, 머스타드가 들어간 버거"));
+        burgerCategory.addMenuItem(new MenuItem("슈슈버거", 0.65, "통새우살 가득한 버거"));
+        burgerCategory.addMenuItem(new MenuItem("불고기버거", 0.5, "불고기 소스와 쇠고기 패티가 어우러진 버거"));
+
+        //Menu 리스트 생성
+        List<Menu> menuList = new ArrayList<>();
+        menuList.add(burgerCategory);
+
+        // Kiosk 객체 생성
+        Kiosk kiosk = new Kiosk(menuList);
+
+        // Kiosk 내 시작하는 함수 호출
+        kiosk.start();
+
+    }
+}
--- src/main/java/com/example/kiosk4/Menu.java
+++ src/main/java/com/example/kiosk4/Menu.java
@@ -1,5 +1,43 @@
 package com.example.kiosk4;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //MenuItem 관리하는 클래스
-public class Menu  {
+public class Menu {
+
+    //List 선언 및 초기화
+    private final List<MenuItem> menuItems;
+
+    //카테고리 선언
+    private final String category;
+
+    //생성자를 통해 값 할당
+    public Menu(String category) {
+        this.category = category;
+        this.menuItems = new ArrayList<>();
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    //리스트를 리턴하는 함수
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
+
+    //리스트에 들어있는 MenuItem을 순차적으로 보여주는 함수
+    public void showMenuItem() {
+        //리스트 안에 있는 MenuItem 하나씩 출력
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.println(i + 1 + ". " + menuItems.get(i));
+        }
+        System.out.println("0. 뒤로가기 | 뒤로가기");
+    }
+
 }
