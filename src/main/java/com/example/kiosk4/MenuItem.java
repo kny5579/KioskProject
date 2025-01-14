ec8d47b189d515ef79d5750d3ba4786a00815aa4
나연 <kny5579@naver.com>
2025-01-14 16:15:46 +0900

    refactor: LV 3,4(변수명 변경)

--- src/main/java/com/example/kiosk4/MenuItem.java
+++ src/main/java/com/example/kiosk4/MenuItem.java
@@ -3,13 +3,13 @@ package com.example.kiosk4;
 //세부 메뉴 속성을 가지는 클래스
 public class MenuItem {
 
-    private final String burgerName;
+    private final String foodName;
     private final double price;
     private final String details;
 
     //생성자
-    public MenuItem(String burgerName, double price, String details) {
-        this.burgerName = burgerName;
+    public MenuItem(String foodName, double price, String details) {
+        this.foodName = foodName;
         this.price = price;
         this.details = details;
     }
@@ -17,6 +17,6 @@ public class MenuItem {
     //메뉴 출력 문구 지정
     @Override
     public String toString() {
-        return String.format("%-15s | w %.1f | %-15s", burgerName, price, details);
+        return String.format("%-15s | w %.1f | %-15s", foodName, price, details);
     }
 }
