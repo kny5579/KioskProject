59929a2eb953089f5e957d4ae909c3a62d43633e
나연 <kny5579@naver.com>
2025-01-21 19:48:25 +0900

    refactor: lv1(while 조건 변경, exception 변경)

--- src/main/java/com/example/lv1/Main.java
+++ src/main/java/com/example/lv1/Main.java
@@ -2,11 +2,10 @@ package com.example.lv1;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean exit = false;
 
-        while (!exit) {
+        while (true) {
             System.out.println("[ SHAKESHACK MENU ]\n" +
                     "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
                     "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
@@ -30,11 +29,10 @@ public class Main {
                     System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n");
                     break;
                 case 0:
-                    exit = true;
                     System.out.println("프로그램을 종료합니다.");
                     break;
                 default:
-                    throw new Exception("메뉴에 없는 번호 선택시 에러. 메시지 변경 예정");
+                    throw new IllegalArgumentException("잘못된 입력입니다.");
             }
         }
     }
